6fc5b61101c67d83aaa515b6f122eebb8bd912f9
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-07-26 09:09:10 +0200

    Revert "sm6150-common: Build ParanoidDoze"
    This reverts commit 97e8fd262c8ab89fbc5c1ec9c427b11b9019f891.

--- sm6150.mk
+++ sm6150.mk
@@ -95,6 +95,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
@@ -218,10 +222,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
-
-# ParanoidDoze
-PRODUCT_PACKAGES += \
-    ParanoidDoze
 
 # Perf
 PRODUCT_PACKAGES += \
